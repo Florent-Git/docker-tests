b2ababebe0e3af17cc6d1aed00fef2a6cc670016
Florent-Git <florent.ram@gmail.com>
2022-11-29 17:14:14 +0100

    Working on semantic versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,27 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.dipien:semantic-version-gradle-plugin:1.3.1")
+    }
+}
+
 plugins {
     application
     kotlin("jvm") version "1.7.21"
     id("io.ktor.plugin") version "2.1.3"
+    id("com.dipien.semantic-version") version "1.3.1" apply false
 }
 
 group = "be.florentgit"
 version = "0.0.1"
+apply<com.semanticversion.gradle.plugin.SemanticVersionGradlePlugin>()
+
 application {
     mainClass.set("be.florentgit.ApplicationKt")
 
